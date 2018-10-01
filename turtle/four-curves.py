9358e54bdb2e52b704801a2d6f4eb91ff6148eab
Chris Harrison <cah@stpaulsschool.org.uk>
2018-10-01 14:54:12 +0100

    4 curves

---
+++ turtle/four-curves.py
@@ -0,0 +1,23 @@
+import turtle
+
+def curves(height, step):
+    wn = turtle.Screen()
+    t = turtle.Turtle()
+    wn.screensize(height*2.1,height*2.1)
+
+    wn.onkey(wn.bye, "q")
+    wn.listen()
+    t.speed(0)
+
+    for x in [-1, 1]:
+        for y in [-1, 1]:
+            for i in range(0, height+1, step):
+                t.penup()
+                t.setpos((0, y*(height-i)))
+                t.pendown()
+                t.goto((x*i,0))
+    
+    t.hideturtle()
+    wn.mainloop()
+
+
