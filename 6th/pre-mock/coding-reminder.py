3b25f35100401ecea688a6f73a4b3db0af698bf7
spscah <cah@stpaulsschool.org.uk>
2019-11-19 12:42:27 +0000

    Tightening code

--- 6th/pre-mock/coding-reminder.py
+++ 6th/pre-mock/coding-reminder.py
@@ -9,32 +9,32 @@ def test(did_pass):
     print(msg)
 
 def sum_to(li):
-	tally = 0 
-	for item in li: 
-		tally += item
-	return tally 
+    tally = 0 
+    for item in li: 
+        tally += item
+    return tally 
 
 test(sum_to([1,2,3,4,6]) == 16)
 
 def find_hypot(a,b):
-	return (a**2 + b**2)**0.5
+    return (a**2 + b**2)**0.5
 
 test(find_hypot(12,5)==13)
 
 def is_rightangled(a,b,c):
-	return find_hypot(a,b)==c or find_hypot(a,c)==b or find_hypot(b,c)==a
+    return find_hypot(a,b)==c or find_hypot(a,c)==b or find_hypot(b,c)==a
 
 test(is_rightangled(12,5,13))
 test(is_rightangled(5,4,3))
 test(not is_rightangled(5,5,3))
 
 def how_many_rightangled(l):
-	return len([x for x,y,z in l if is_rightangled(x,y,z)])
+    return len([x for x,y,z in l if is_rightangled(x,y,z)])
 
 test(how_many_rightangled([(1,3,2),(3,4,5),(12,13,5), (5,4,6)]) == 2)
 
 def to_secs(h,m,s):
-	return (((h*60)+m)*60)+s
+    return (((h*60)+m)*60)+s
 
 test(to_secs(2, 30, 10) == 9010)
 test(to_secs(2, 0, 0) == 7200)
@@ -43,36 +43,36 @@ test(to_secs(0, 0, 42) == 42)
 test(to_secs(0, -10, 10) == -590)
 
 def since_2000(s):
-	def day_calc():
-		return 24 * 60 * 60
+    def day_calc():
+        return 24 * 60 * 60
 
-	def year_calc(y):
-		return (365 + (1 if y % 4 == 0 else 0)) * day_calc()
+    def year_calc(y):
+        return (365 + (1 if y % 4 == 0 else 0)) * day_calc()
 
-	def month_calc(m, y):
-		f = 29 if y%4 == 0 else 28
-		return [31,f,31,30,31,30,31,31,30,31,30,31][m] * day_calc()
+    def month_calc(m, y):
+        f = 29 if y%4 == 0 else 28
+        return [31,f,31,30,31,30,31,31,30,31,30,31][m] * day_calc()
 
-	y = 2000 
-	while s > year_calc(y):
-		s -= year_calc(y)
-		y +=1 
+    y = 2000 
+    while s > year_calc(y):
+        s -= year_calc(y)
+        y +=1 
 
-	m = 0
-	while s > month_calc(m,y):
-		s-= month_calc(m,y)
-		m+= 1
-	d = (s // day_calc()) + 1
-	e = 'th'
-	if d in [1,21,31]:
-		e = 'st'
-	elif d in [2,22]:
-		e = 'nd'
-	elif d in [3,23]:
-		e = 'rd'
-	return "{day}{ending} {month}, {year}".format(ending=e,day=d,year=y,month=['January','February','March','April','May','June','July','August','September','October','November','December'][m])
+    m = 0
+    while s > month_calc(m,y):
+        s-= month_calc(m,y)
+        m+= 1
+    d = (s // day_calc()) + 1
+    e = 'th'
+    if d in [1,21,31]:
+        e = 'st'
+    elif d in [2,22]:
+        e = 'nd'
+    elif d in [3,23]:
+        e = 'rd'
+    return "{day}{ending} {month}, {year}".format(ending=e,day=d,year=y,month=['January','February','March','April','May','June','July','August','September','October','November','December'][m])
 
 print(since_2000(1000000))
 print(since_2000(10000000))
 print(since_2000(100000000))
-
+print(since_2000(31590115200))
--- 6th/pre-mock/competition-medals.py
+++ 6th/pre-mock/competition-medals.py
@@ -1,20 +1,11 @@
 people = [4244, 4004, 3091, 4275, 4104, 3211, 3400, 3800, 4014, 3025, 3490, 3256]
 rounds = [[67, 72, 51, 69, 42, 87, 62, 81, 84, 51, 85, 62] , [25, 40, 29, 56, 83, 77, 82, 31, 52, 90, 66, 93], [100, 48, 35, 68, 53, 48, 96, 63, 38, 77, 33, 71]]
-
 means = [sum(rounds[r])/len(rounds[r]) for r in range(len(rounds))]
-for r in range(len(rounds)):
-	print("mean for round {0} is {1:.1f}".format(r+1,means[r]))
+print('\n'.join(["mean for round {0} is {1:.1f}".format(r+1,means[r]) for r in range(len(rounds))]))
 personmaps = [[rounds[m][p] for m in range(3)] for p in range(len(rounds[0]))]
-
 personaltotals = [sum(i) for i in personmaps]
-
 sortedtotals = personaltotals[:]
 sortedtotals.sort(reverse=True)
-for m in range(3):
-	i = personaltotals.index(sortedtotals[m])
-	print("{0}: person {1} with {2}".format(['Gold', 'Silver','Bronze'][m], people[i],personaltotals[i]))
-
+print(', '.join(["{0}: person {1} with {2}".format(['Gold', 'Silver','Bronze'][m], people[personaltotals.index(sortedtotals[m])],personaltotals[personaltotals.index(sortedtotals[m])]) for m in range(3)]))
 filtered = [len([i for i in range(len(pm)) if pm[i] >= means[i]]) == len(pm) for pm in personmaps]
-
 print("Special prize winners: {0}".format('none' if len([f for f in filtered if f]) == 0 else ', '.join(str(people[i]) for i in range(len(people)) if filtered[i])))
-
